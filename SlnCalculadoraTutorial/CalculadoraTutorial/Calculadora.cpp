4915ca13234d0fa2517534eed51d8d36b5e19db9
andrea <andreavs05@hotmail.com>
2019-05-14 13:00:43 -0500

    Se añadio metodo calcular

---
+++ SlnCalculadoraTutorial/CalculadoraTutorial/Calculadora.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "Calculadora.h"
+
+/*
+Calculadora::Calculadora()
+{
+}
+
+
+Calculadora::~Calculadora()
+{
+}
+*/
+double Calculadora::Calculate(double x, char oper, double y)
+{
+	return 0.0;
+}
---
+++ SlnCalculadoraTutorial/CalculadoraTutorial/Calculadora.h
@@ -0,0 +1,9 @@
+#pragma once
+class Calculadora
+{
+public:
+	//Calculadora();//constructor
+	//~Calculadora();//destrucctor
+	double Calculate(double x, char oper, double y);
+};
+
